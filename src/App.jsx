37b4f62f35f380ad0d7d976272dc7ec09672d877
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-03-19 17:36:13 +0300

    feat:set up react query and fetch cabin data

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,27 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
-
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+//! Create place where data lives
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      //! update time
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -32,7 +47,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 

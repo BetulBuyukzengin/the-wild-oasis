80bab8b5b473ef48824aa2525146f16d64163b6a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-19 01:07:14 +0300

    feat: add usetranslation to translate

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -10,12 +10,13 @@ import Textarea from "../../ui/Textarea";
 import FormRow from "../../ui/FormRow";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
+import { useTranslation } from "react-i18next";
 
 function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   //* use custom hook
   const { isCreating, createCabin } = useCreateCabin();
   const { isEditing, editCabin } = useEditCabin();
-
+  const { t } = useTranslation();
   const { id: editId, ...editValues } = cabinToEdit;
   //! get default value when click on edit button
   const isEditSession = Boolean(editId);
@@ -69,70 +70,73 @@ function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
           type="text"
           id="name"
           {...register("name", {
-            required: "This field is required",
+            required: t("This field is required!"),
           })}
         />
         {errors?.name?.message && <Error>{errors.name.message}</Error>}
       </FormRow> */}
 
-      <FormRow label="Cabin name" error={errors?.name?.message}>
+      <FormRow label={t("Cabin name")} error={errors?.name?.message}>
         <Input
           type="text"
           id="name"
           disabled={isWorking}
           {...register("name", {
-            required: "This field is required",
+            required: t("This field is required!"),
           })}
         />
       </FormRow>
 
-      <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
+      <FormRow
+        label={t("Maximum capacity")}
+        error={errors?.maxCapacity?.message}
+      >
         <Input
           type="number"
           id="maxCapacity"
           disabled={isWorking}
           {...register("maxCapacity", {
-            required: "This field is required",
+            required: t("This field is required!"),
             min: {
               value: 1,
-              message: "Capacity should be at least 1",
+              message: t("Capacity should be at least 1!"),
             },
           })}
         />
       </FormRow>
 
-      <FormRow label="Regular Price" error={errors?.regularPrice?.message}>
+      <FormRow label={t("Regular Price")} error={errors?.regularPrice?.message}>
         <Input
           type="number"
           id="regularPrice"
           disabled={isWorking}
           {...register("regularPrice", {
-            required: "This field is required",
+            required: t("This field is required!"),
             min: {
               value: 1,
-              message: "Regular price should be at least 1",
+              message: t("Regular price should be at least 1!"),
             },
           })}
         />
       </FormRow>
 
-      <FormRow label="Discount" error={errors?.discount?.message}>
+      <FormRow label={t("Discount")} error={errors?.discount?.message}>
         <Input
           type="number"
           id="discount"
           disabled={isWorking}
           defaultValue={0}
           {...register("discount", {
-            required: "This field is required",
+            required: t("This field is required!"),
             validate: (value) =>
               +value <= +getValues().regularPrice ||
-              "Discount should be less than regular price",
+              t("Discount should be less than regular price!"),
           })}
         />
       </FormRow>
 
       <FormRow
-        label="Description for website"
+        label={t("Description for website")}
         error={errors?.description?.message}
       >
         <Textarea
@@ -140,13 +144,13 @@ function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
           id="description"
           defaultValue=""
           {...register("description", {
-            required: "This field is required",
+            required: t("This field is required!"),
           })}
         />
       </FormRow>
 
       <FormRow
-        label="Cabin photo"
+        label={t("Cabin photo")}
         error={errors?.image?.message}
         disabled={isWorking}
       >
@@ -155,7 +159,7 @@ function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
           accept="image/*"
           type="file"
           {...register("image", {
-            required: isEditSession ? false : "This field is required",
+            required: isEditSession ? false : t("This field is required!"),
           })}
         />
       </FormRow>
@@ -167,10 +171,10 @@ function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
           type="reset"
           onClick={() => onCloseModal?.()}
         >
-          Cancel
+          {t("Cancel")}
         </Button>
         <Button disabled={isWorking}>
-          {isEditSession ? "Edit cabin" : " Create new cabin"}
+          {isEditSession ? t("Edit cabin") : t("Create new cabin")}
         </Button>
       </FormRow>
     </Form>

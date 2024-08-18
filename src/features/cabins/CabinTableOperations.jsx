50ce7c8fc1658c2922e2a9bdf1663019595474da
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-19 01:07:20 +0300

    feat: add usetranslation to translate

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,26 +1,37 @@
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
 import SortBy from "../../ui/SortBy";
+import { useTranslation } from "react-i18next";
 
 function CabinTableOperations() {
+  const { t } = useTranslation();
   return (
     <TableOperations>
       <Filter
         filterField="discount"
         options={[
-          { value: "all", label: "All" },
-          { value: "no-discount", label: "No discount" },
-          { value: "with-discount", label: "Withdiscount" },
+          { value: "all", label: t("All") },
+          { value: "no-discount", label: t("No discount") },
+          { value: "with-discount", label: t("With discount") },
         ]}
       />
       <SortBy
         options={[
-          { value: "name-asc", label: "Sort by name (A-Z)" },
-          { value: "name-desc", label: "Sort by name (Z-A)" },
-          { value: "regularPrice-asc", label: "Sort by price (low first)" },
-          { value: "regularPrice-desc", label: "Sort by price (high first)" },
-          { value: "maxCapacity-asc", label: "Sort by capacity (low first)" },
-          { value: "maxCapacity-desc", label: "Sort by capacity (high first)" },
+          { value: "name-asc", label: t("Sort by name (A-Z)") },
+          { value: "name-desc", label: t("Sort by name (Z-A)") },
+          { value: "regularPrice-asc", label: t("Sort by price (low first)") },
+          {
+            value: "regularPrice-desc",
+            label: t("Sort by price (high first)"),
+          },
+          {
+            value: "maxCapacity-asc",
+            label: t("Sort by capacity (low first)"),
+          },
+          {
+            value: "maxCapacity-desc",
+            label: t("Sort by capacity (high first)"),
+          },
         ]}
       />
     </TableOperations>

538c3f31c2db771c4e964524044e046c8499fda1
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-08-18 17:41:36 +0300

    chore: remove tanstack query tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
-import Users from "./pages/Users";
-import Settings from "./pages/Settings";
-import Account from "./pages/Account";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import ProtectedRoute from "./ui/ProtectedRoute";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
+import { DarkModeProvider } from "./context/DarkModeContext";
 import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin";
-import { DarkModeProvider } from "./context/DarkModeContext";
 
 //! Create place where data lives
 const queryClient = new QueryClient({
@@ -34,7 +32,6 @@ function App() {
   return (
     <DarkModeProvider>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
         <GlobalStyles />
         <BrowserRouter>
           <Routes>
